4845d597a5eaf1b991b09fc5214d32403e10d296
ydvorzhetskiy <dvorzhetskij@mail.ru>
2019-02-06 16:58:03 +0600

    Generated project

---
+++ src/main/java/ru/otus/demoserver/DemoServerApplication.java
@@ -0,0 +1,14 @@
+package ru.otus.demoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoServerApplication.class, args);
+	}
+
+}
+
